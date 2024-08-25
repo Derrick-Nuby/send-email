b871783eeeb9f4ee0b222d22ebfc422fe1b100b6
Derrick-Nuby <iradukundaderrick7@gmail.com>
2024-08-25 23:50:44 +0200

    some ignored files recovered

---
+++ src/data/emailTemplates.ts
@@ -0,0 +1,6 @@
+export const emailSubject = 'Thank You for Your Application';
+
+export const emailContent = (name: string) => `
+    <h1>Hello ${name}</h1>
+    <p>Thank you for your interest in our program</p>
+`;
---
+++ src/data/userEmails.ts
@@ -0,0 +1,8 @@
+const userEmails = [
+    {
+        name: "Melody Apifan",
+        email: "meledol717@apifan.com"
+    }
+];
+
+export default userEmails;
